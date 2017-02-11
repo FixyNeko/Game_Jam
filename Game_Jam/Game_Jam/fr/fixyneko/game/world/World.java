2af8d3c07811de067ab34a660b4bb56ce570b8b8
Felix <Felix@172.27.140.32>
2017-02-11 11:26:34 +0100

    wip

--- Game_Jam/Game_Jam/fr/fixyneko/game/world/World.java
+++ Game_Jam/Game_Jam/fr/fixyneko/game/world/World.java
@@ -4,18 +4,26 @@ import java.awt.Graphics;
 
 import fr.gagoi.game.graphics.Drawable;
 
-public class World implements Drawable{
+public class World implements Drawable {
 
 	Case[][] cases;
-	
+	int scl;
+	int gameX = 800;
+	int gameY = 800;
+
 	public World(int x, int y) {
 		cases = new Case[x][y];
+		scl = Math.min(gameX/x, gameY/y);
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
-		
+		for (int x = 0; x < cases.length; x++) {
+			for (int y = 0; y < cases[x].length; y++) {
+				g.drawImage(cases[x][y].getImage(), x * scl, y * scl, null);
+			}
+		}
 	}
 
 }
