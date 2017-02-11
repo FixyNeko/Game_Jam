6e6723d05e5020dd923df8b853ac30431636c3eb
Felix <Felix@172.27.140.32>
2017-02-11 12:37:19 +0100

    world init

--- Game_Jam/Game_Jam/fr/fixyneko/game/world/World.java
+++ Game_Jam/Game_Jam/fr/fixyneko/game/world/World.java
@@ -1,6 +1,7 @@
 package fr.fixyneko.game.world;
 
 import java.awt.Graphics;
+import java.util.Random;
 
 import fr.gagoi.game.core.Game;
 import fr.gagoi.game.graphics.Drawable;
@@ -12,10 +13,26 @@ public class World implements Drawable {
 	int gameX = 800;
 	int gameY = 800;
 
+	int ressources[] = { 10, 10, 10, 10, 10 };
+
 	public World(int x, int y, int scl_) {
-		cases = new Case[x][y];
+		// cases = new Case[x][y];
 		scl = scl_;
 		Game.GAME.getDisplay().getCanvas().getDrawables().add(this);
+	}
+
+	void init() {
+		for (int i = 0; i < ressources.length; i++) {
+			int x;
+			int y;
+			
+			do {
+				x = new Random().nextInt(gameX - 1);
+				y = new Random().nextInt(gameY - 1);
+			} while (cases[x][y] == null);
+			
+			cases[x][y] = new Case(i);
+		}
 	}
 
 	@Override
