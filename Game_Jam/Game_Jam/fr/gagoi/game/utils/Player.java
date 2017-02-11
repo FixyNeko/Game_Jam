46c5166719faaeba80733463b4024d21f2f3a695
gagoi <gagoi@gagoi-GP60-2QE>
2017-02-11 16:22:31 +0100

    Debug

--- Game_Jam/Game_Jam/fr/gagoi/game/utils/Player.java
+++ Game_Jam/Game_Jam/fr/gagoi/game/utils/Player.java
@@ -8,13 +8,13 @@ public class Player {
 
 	private String name;
 	private int id;
-	private DeckCard[] deck;
+	//private DeckCard[] deck;
 	private ResourceList resources;
 
 	public Player(int playerId, int character, String name) {
 		this.id = playerId;
 		this.name = name;
-		this.deck=new DeckCard[]{new DeckCard(0),new DeckCard(1),new DeckCard(2),new DeckCard(3),new DeckCard(4),new DeckCard(5)};
+		//this.deck=new DeckCard[]{new DeckCard(0),new DeckCard(1),new DeckCard(2),new DeckCard(3),new DeckCard(4),new DeckCard(5)};
 		this.resources = new ResourceList();
 		for (int i = 0; i < base_resources[character].length; i++)
 			this.resources.setResource(i, base_resources[character][i]);
