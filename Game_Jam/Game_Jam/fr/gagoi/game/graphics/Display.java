52dde7cbe837cf0e56476be279800ae0b8cf956d
Felix <Felix@172.27.140.32>
2017-02-11 14:25:06 +0100

    fps stabilisation

--- Game_Jam/Game_Jam/fr/gagoi/game/graphics/Display.java
+++ Game_Jam/Game_Jam/fr/gagoi/game/graphics/Display.java
@@ -26,28 +26,35 @@ public class Display extends JFrame implements Runnable {
 
 	@Override
 	public void run() {
-		long startTime = System.currentTimeMillis(), startTimeFPS = System.currentTimeMillis();
+		long startTimeFPS = System.currentTimeMillis();
+		// System.currentTimeMillis();
+		// long lastTimeFrame = System.currentTimeMillis();
+		// long lastFrameTime = System.currentTimeMillis();
 
 		while (isRunning) {
-			long currentTime = System.currentTimeMillis(), currentTimeFPS = System.currentTimeMillis();
-			long delta = currentTime - startTime, deltaFPS = currentTimeFPS - startTimeFPS;
-			if (delta >= 100 / 6) {
-				canvas.render();
-				startTime = System.currentTimeMillis();
+
+			long renderBegin = System.currentTimeMillis();
+			canvas.render();
+			long renderEnd = System.currentTimeMillis();
+			
+
+			long t = 1000 / 60 - (renderEnd - renderBegin);
+			if (t > 0)
 				try {
-					long t = 100 / 6 - delta;
-					if (t > 0)
-						Thread.sleep(100 / 6 - delta);
+					Thread.sleep(t);
 				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				fps++;
-			}
+
+			long deltaFPS = System.currentTimeMillis() - startTimeFPS;
+			fps++;
 			if (deltaFPS >= 1000) {
 				setTitle(TITLE + " | fps=" + fps);
 				startTimeFPS = System.currentTimeMillis();
 				fps = 0;
 			}
+
 		}
 	}
 
